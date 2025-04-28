70cddecd8e470978753a704d901e4af101adeb71
Kiran Patil <kiran2912.patil@gmail.com>
2025-04-28 14:02:48 +0530

    Feature/create login tests rebase done merging now (#7)
    * added improvements in basePage
    
    * made it pretty
    
    * retry logic
    
    * added gitWorkFlow file
    
    * commented playwright config file code
    
    ---------
    
    Co-authored-by: Kiran Patil <kiran@Kirans-MacBook-Air.local>

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-//  / retries: process.env.CI ? 1 : 2,
+  retries: process.env.CI ? 1 : 2,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -46,15 +46,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
