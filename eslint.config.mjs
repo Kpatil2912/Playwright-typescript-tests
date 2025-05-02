1e9a4643bee1baa0bf1056ec8109ecb0857cc9f6
Kiran Patil <kiranqapatil@gmail.com>
2025-05-03 00:41:40 +0530

    chore: eslint config file update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,24 +3,27 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
 
-export default defineConfig([
+export default defineConfig(
+  [
+    {
+      files: ['**/*.{js,mjs,cjs,ts}'],
+      plugins: { js },
+      extends: ['js/recommended'],
+    },
+    { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+    {
+      files: ['**/*.{js,mjs,cjs,ts}'],
+      languageOptions: { globals: globals.browser },
+    },
+    tseslint.configs.recommended,
+  ],
   {
-    files: ['**/*.{js,mjs,cjs,ts}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-  },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  {
-    files: ['**/*.{js,mjs,cjs,ts}'],
-    languageOptions: { globals: globals.browser },
-  },
-  tseslint.configs.recommended,
-], {
-  ignores: [
-    'commitlint.config.js', // ignore config files
-    'node_modules',
-    'playwright-report',
-    'test-results',
-    'dist'
-  ]
-});
+    ignores: [
+      'commitlint.config.js', // ignore config files
+      'node_modules',
+      'playwright-report',
+      'test-results',
+      'dist',
+    ],
+  }
+);
